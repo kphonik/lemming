113470a098729cea3870d2a06cf7b521affc416a
Adam J. Weigold <adam@adamweigold.com>
2015-03-29 14:38:29 -0500

    Added missing quote

--- .travis/deploy_ghpages.sh
+++ .travis/deploy_ghpages.sh
@@ -5,5 +5,5 @@ git config user.name "travis-ci"
 git config user.email "travis@travis-ci.org"
 git add .
 git commit -m "Publishing site from Travis CI build $TRAVIS_BUILD_NUMBER"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF} master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 echo "Published site to gh-pages.  See http://aweigold.github.io/lemming"
